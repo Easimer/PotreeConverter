ebff74cedd79bd3e67aa8e70ead00a23e612185f
Daniel Meszaros <daniel.meszaros@r34dy.io>
2025-08-27 11:54:54 +0200

    Find tbb through pkg-config; build on Nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "PotreeConverter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            cmake
+            tbb_2020
+            pkg-config
+          ];
+        };
+      }
+    );
+}
