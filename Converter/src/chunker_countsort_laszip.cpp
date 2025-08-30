e617252d482d256e4a1d92cf212d56554bae6117
Dániel Mészáros <daniel.meszaros@maxwhere.com>
2025-08-30 08:29:38 +0200

    Cap batch size during counting

--- Converter/src/chunker_countsort_laszip.cpp
+++ Converter/src/chunker_countsort_laszip.cpp
@@ -301,7 +301,8 @@ namespace chunker_countsort_laszip {
 
 			int64_t pointsLeft = numPoints;
 			// int64_t batchSize = 1'000'000;
-			int64_t batchSize = std::max(size_t(1), numPoints / (2 * std::max(numChunkerThreads, size_t(1))));
+			size_t batchSize = numPoints / (2 * std::max(numChunkerThreads, size_t(1)));
+			batchSize = std::max(size_t(1), std::min(batchSize, size_t(16777216)));
 			int64_t numRead = 0;
 
 			while (pointsLeft > 0) {
